19422c907c156bd4774ce357007144a7fad9971c
gabrielo <gabrielorteganicolas@gmail.com>
2025-03-06 13:33:29 +0100

    task class init

---
+++ task-manager/src/main/java/com/victor/tasks/Task.java
@@ -0,0 +1,24 @@
+package com.victor.tasks;
+
+import java.util.UUID;
+
+public class Task {
+    String id;
+    String title;
+    String description;
+    String category;
+    TaskState taskState;
+
+    public Task(String title, String description, String category) {
+        this.id = UUID.randomUUID().toString();
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.taskState = TaskState.TODO;
+    }
+
+    
+
+
+
+}
---
+++ task-manager/src/main/java/com/victor/tasks/TaskState.java
@@ -0,0 +1,7 @@
+package com.victor.tasks;
+
+public enum TaskState {
+    TODO, 
+    IN_PROGRESS,
+    DONE
+}
